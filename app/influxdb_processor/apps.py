f8f672c27224d5b580c5690adaac1de0e1219ba9
zhitko <zhitko.vladimir@gmail.com>
2024-12-12 14:32:46 +0300

    clean

--- app/influxdb_processor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InfluxdbProcessorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'influxdb_processor'
--- app/proxmox/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django import forms
-
-from .models import Provider
-
-admin.site.register(Provider)
